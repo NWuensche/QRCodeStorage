631a4431b51ddf84b3d2c64a0fd6d0636ffa1506
nwuensche <Wuensche.Niklas@gmail.com>
2021-05-21 08:54:14 +0200

    Add Recycler View and fill it

--- app/src/main/java/de/nwuensche/qrcodestorage/MainActivity.kt
+++ app/src/main/java/de/nwuensche/qrcodestorage/MainActivity.kt
@@ -3,16 +3,23 @@ package de.nwuensche.qrcodestorage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.budiyev.android.codescanner.*
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var codeScanner: CodeScanner
-
+    //TODO Licenses Libraries
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rView = findViewById<RecyclerView>(R.id.qrlist).apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = RAdapter()
+        }
 
         val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         fab.setOnClickListener {
---
+++ app/src/main/java/de/nwuensche/qrcodestorage/RAdapter.kt
@@ -0,0 +1,21 @@
+package de.nwuensche.qrcodestorage
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RAdapter : RecyclerView.Adapter<RAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = 30
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {}
+}
